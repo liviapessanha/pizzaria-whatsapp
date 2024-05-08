32eb2bb327d4951db33cfa1fdec3730901f06ace
Livia Pessanha <liviapessanha99@gmail.com>
2024-05-08 09:33:47 -0300

    changed footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,14 @@
 import { Separator } from "@/components/ui/separator"
+import { Copyright } from "lucide-react"
 
 export const Footer = () => {
   return (
     <footer className="mt-5">
     <Separator />
-    <p className="my-5 text-center text-sm opacity-50">Criado por Lívia Pessanha</p>
+      <p className="my-5 text-center text-sm opacity-50 flex items-center justify-center">
+      
+        <Copyright size={14} />  Copyright 2024. Criado por Lívia Pessanha
+      </p>
   </footer>
   )
   
